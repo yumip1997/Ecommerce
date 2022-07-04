8938a4acf53e5ba8503fa1d0d4bd7f39332a37c7
YUMI <yumip1997@gmail.com>
2022-07-04 10:47:15 +0900

    기간 구분코드 추가

---
+++ src/main/java/com/plateer/ec1/promotion/enums/PRM0002Code.java
@@ -0,0 +1,13 @@
+package com.plateer.ec1.promotion.enums;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public enum PRM0002Code {
+
+    PRD("10"),
+    DT("20");
+
+    private final String code;
+
+}
