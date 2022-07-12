170540f4bf7c26de1355407fcf7fb742d2c9b0c2
YUMI <yumip1997@gmail.com>
2022-07-12 09:21:46 +0900

    포인트 컷 수정

--- src/main/java/com/plateer/ec1/common/aop/LoginIdSettingAspect.java
+++ src/main/java/com/plateer/ec1/common/aop/LoginIdSettingAspect.java
@@ -2,24 +2,20 @@ package com.plateer.ec1.common.aop;
 
 import com.plateer.ec1.common.vo.BaseVO;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
-import java.util.Collection;
 import java.util.List;
 
 @Component
 @Aspect
 public class LoginIdSettingAspect {
 
-    @Before("@annotation(LoginIdSetting)")
+    @Before("execution(* *..*TrxMapper.*(..))")
     public void setLogindId(JoinPoint joinPoint){
         Object[] args = joinPoint.getArgs();
         for (Object arg : args) {
-            //TODO 수정 필요
             if(arg instanceof List){
                 for (Object o : (List) arg) {
                     setLoginIdOfArgs(o);
--- src/main/java/com/plateer/ec1/promotion/download/mapper/CupDwlTrxMapper.java
+++ src/main/java/com/plateer/ec1/promotion/download/mapper/CupDwlTrxMapper.java
@@ -1,16 +1,13 @@
 package com.plateer.ec1.promotion.download.mapper;
 
-import com.plateer.ec1.common.aop.LoginIdSetting;
 import com.plateer.ec1.common.model.promotion.CcCpnIssueModel;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface CupDwlTrxMapper {
 
-    @LoginIdSetting
     void insertCup(CcCpnIssueModel ccCpnIssueModel);
 
-    @LoginIdSetting
     void updateCup(CcCpnIssueModel ccCpnIssueModel);
 
 }
