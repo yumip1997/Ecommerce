595aee03cccbca12aba83a40c808157fb2b38df4
yumip1997 <yumip1997@gmail.com>
2022-07-14 11:17:35 +0900

    가상계좌 채번 로직 개발 중

--- src/main/java/com/plateer/ec1/payment/controller/InicisController.java
+++ src/main/java/com/plateer/ec1/payment/controller/InicisController.java
@@ -1,13 +1,11 @@
 package com.plateer.ec1.payment.controller;
 
-import com.plateer.ec1.common.aop.ResValid;
 import com.plateer.ec1.common.utils.HttpServletRequestUtil;
 import com.plateer.ec1.payment.processor.impl.InicisProcessor;
 import com.plateer.ec1.payment.utils.InicisApiConstants;
 import com.plateer.ec1.payment.vo.res.VacctDpstCmtResResVO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +19,7 @@ public class InicisController {
     private final InicisProcessor inicisProcessor;
 
     @PostMapping("/vcaatDeposit")
-    public void completeVacctDeposit(@ResValid VacctDpstCmtResResVO data, HttpServletRequest req){
+    public void completeVacctDeposit(VacctDpstCmtResResVO data, HttpServletRequest req){
         String clientIp = HttpServletRequestUtil.getClientIP(req);
         if(!InicisApiConstants.VACCT_DPST_NTC_LIST.contains(clientIp)) return;
 
