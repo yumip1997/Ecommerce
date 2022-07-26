06ab1082f38b9a6f0ec736a14206e51f1629e183
YUMI <yumip1997@gmail.com>
2022-07-26 16:27:20 +0900

    json 변경 추가

--- src/main/java/com/plateer/ec1/common/utils/ObjectMapperUtil.java
+++ src/main/java/com/plateer/ec1/common/utils/ObjectMapperUtil.java
@@ -1,6 +1,5 @@
 package com.plateer.ec1.common.utils;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
@@ -14,16 +13,19 @@ public class ObjectMapperUtil {
     private final static ObjectMapper mapper = new ObjectMapper();
     private final static Gson gson = new Gson();
 
-    public static <T> MultiValueMap<String, String> convertMultiValueMap(T t){
+    public static <T> MultiValueMap<String, String> convertMultiValueMap(T t) {
         Map<String, String> map = mapper.convertValue(t, new TypeReference<Map<String, String>>() {});
         MultiValueMap<String, String> multiValueMap = new LinkedMultiValueMap<>();
         multiValueMap.setAll(map);
         return multiValueMap;
     }
 
-    public static <T> T convertJsonIntoObject(String str, Class<T> type){
+    public static <T> T convertJsonIntoObject(String str, Class<T> type) {
         return gson.fromJson(str, type);
     }
 
+    public static <T> String toJson(T obj){
+        return gson.toJson(obj);
+    }
 
 }
