015412a6fecad494d9a996bb4a59f9dea19e2ce2
YUMI <yumip1997@gmail.com>
2022-07-26 10:45:30 +0900

    이름 변경

--- src/main/java/com/plateer/ec1/claim/mapper/LogDao.java
+++ src/main/java/com/plateer/ec1/claim/mapper/LogDao.java
@@ -2,6 +2,7 @@ package com.plateer.ec1.claim.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 
+//TODO 삭제예정
 @Mapper
 public interface LogDao {
     Long insertMonitoringLog(String json);
--- src/main/java/com/plateer/ec1/order/mapper/OrdTrxMapper.java
+++ src/main/java/com/plateer/ec1/order/mapper/OrdTrxMapper.java
@@ -4,7 +4,7 @@ import com.plateer.ec1.order.vo.OrderVO;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
-public interface OrderDao {
+public interface OrdTrxMapper {
 
     void insertOrderData(OrderVO orderVO);
 }
