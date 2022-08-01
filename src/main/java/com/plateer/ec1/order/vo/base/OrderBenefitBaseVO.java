0ecce6e376c88c57151af449cc6d85078094df77
YUMI <yumip1997@gmail.com>
2022-08-01 17:02:14 +0900

    수정

--- src/main/java/com/plateer/ec1/order/vo/base/OrderBenefitBaseVO.java
+++ src/main/java/com/plateer/ec1/order/vo/base/OrderBenefitBaseVO.java
@@ -1,5 +1,6 @@
 package com.plateer.ec1.order.vo.base;
 
+import com.plateer.ec1.common.model.order.OpOrdBnfRelInfo;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,7 +15,7 @@ public class OrderBenefitBaseVO {
     private Long prmNo;
     private Long cpnIssNo;
     private String cpnKndCd;
-    private int degrCcd;
-    private int ordBnfAmt;
+    private String degrCcd;
+    private long aplyAmt;
 
 }
