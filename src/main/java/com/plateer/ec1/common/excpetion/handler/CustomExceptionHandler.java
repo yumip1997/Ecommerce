7f00dffbd32780bfa7258667006b5154140285a9
YUMI <yumip1997@gmail.com>
2022-08-16 15:47:31 +0900

    ResponseEntity 수정

--- src/main/java/com/plateer/ec1/common/excpetion/handler/CustomExceptionHandler.java
+++ src/main/java/com/plateer/ec1/common/excpetion/handler/CustomExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.plateer.ec1.common.excpetion.handler;
 
 import com.plateer.ec1.common.excpetion.custom.BusinessException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class CustomExceptionHandler {
 
     @ExceptionHandler({BusinessException.class})
-    public ResponseEntity handleBusinessException(RuntimeException runtimeException){
-        return ResponseEntity.internalServerError().body(runtimeException.getMessage());
+    public ResponseEntity<String> handleBusinessException(RuntimeException runtimeException){
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(runtimeException.getMessage());
     }
 }
---
+++ src/main/java/com/plateer/ec1/common/utils/ResponseEntityUtil.java
@@ -0,0 +1,12 @@
+package com.plateer.ec1.common.utils;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+import java.nio.charset.Charset;
+
+public class ResponseEntityUtil {
+
+}
