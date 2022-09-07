d6734d8b20c0ef0522191092e06ff6ca953cd8a3
yumip1997 <yumip1997@gmail.com>
2022-09-07 16:25:01 +0900

    트랜잭션 수정

--- src/main/java/com/plateer/ec1/claim/externals/impl/PaymentIFCallHelper.java
+++ src/main/java/com/plateer/ec1/claim/externals/impl/PaymentIFCallHelper.java
@@ -5,14 +5,11 @@ import com.plateer.ec1.claim.externals.ExternalIFCallHelper;
 import com.plateer.ec1.claim.vo.ClaimInsertBase;
 import com.plateer.ec1.claim.vo.ClaimRequestVO;
 import com.plateer.ec1.claim.vo.ClaimUpdateBase;
-import com.plateer.ec1.common.factory.MultiValueCustomFactory;
 import com.plateer.ec1.order.vo.OrdClmCreationVO;
 import com.plateer.ec1.payment.service.PayService;
 import com.plateer.ec1.payment.vo.req.PaymentCancelReqVO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Arrays;
 import java.util.List;
@@ -28,7 +25,6 @@ public class PaymentIFCallHelper implements ExternalIFCallHelper {
         return Arrays.asList(ClaimBusiness.GCC, ClaimBusiness.MCC);
     }
 
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
     @Override
     public void call(ClaimRequestVO claimRequestVO, OrdClmCreationVO<ClaimInsertBase, ClaimUpdateBase> creationVO) {
         PaymentCancelReqVO paymentCancelReqVO = claimRequestVO.toPaymentCancelReqVO(creationVO.getClmNo());
