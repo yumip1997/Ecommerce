b2261e1874ffec3870e3462c22be0dac472b8325
yumip1997 <yumip1997@gmail.com>
2022-09-07 16:28:57 +0900

    유효성 검증 수정

--- src/main/java/com/plateer/ec1/order/vo/base/OrderClaimBaseVO.java
+++ src/main/java/com/plateer/ec1/order/vo/base/OrderClaimBaseVO.java
@@ -2,12 +2,15 @@ package com.plateer.ec1.order.vo.base;
 
 import lombok.*;
 
+import javax.validation.constraints.NotEmpty;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class OrderClaimBaseVO {
 
+    @NotEmpty
     private String ordNo;
     private String clmNo;
 
